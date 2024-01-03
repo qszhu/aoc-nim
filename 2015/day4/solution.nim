fc19286d5be89a8500840e7f1f16a94f0c5d19bd
Qinsi ZHU <qszhu2006@gmail.com>
2024-01-03 18:11:26 +0800

    2015 day4

---
+++ 2015/day4/solution.nim
@@ -0,0 +1,29 @@
+import std/[
+  algorithm,
+  sequtils,
+  sets,
+  strformat,
+  strutils,
+]
+
+import checksums/md5
+
+
+
+proc mine(prefix: string, zeros: int): int =
+  while not getMD5(&"{prefix}{result}").startsWith("0".repeat(zeros)):
+    result += 1
+
+proc part1(input: string): int =
+  mine(input, 5)
+
+doAssert part1("abcdef") == 609043
+doAssert part1("pqrstuv") == 1048970
+
+proc part2(input: string): int =
+  mine(input, 6)
+
+when isMainModule:
+  let input = readAll(stdin).strip
+  echo part1(input)
+  echo part2(input)
