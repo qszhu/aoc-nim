718fa83b609f578537ad86e4a9a370d82087f822
Qinsi ZHU <qszhu2006@gmail.com>
2024-01-03 13:56:58 +0800

    2015 day1

---
+++ 2015/day1/solution.nim
@@ -0,0 +1,35 @@
+import std/[
+  strutils,
+]
+
+
+
+proc part1(s: string): int =
+  for c in s:
+    if c == '(': result += 1
+    else: result -= 1
+
+doAssert part1("(())") == 0
+doAssert part1("()()") == 0
+doAssert part1("(((") == 3
+doAssert part1("(()(()(") == 3
+doAssert part1("))(((((") == 3
+doAssert part1("())") == -1
+doAssert part1("))(") == -1
+doAssert part1(")))") == -3
+doAssert part1(")())())") == -3
+
+proc part2(s: string): int =
+  var f = 0
+  for i, c in s:
+    if c == '(': f += 1
+    else: f -= 1
+    if f == -1: return i + 1
+
+doAssert part2(")") == 1
+doAssert part2("()())") == 5
+
+when isMainModule:
+  let input = readAll(stdin).strip
+  echo part1(input)
+  echo part2(input)
