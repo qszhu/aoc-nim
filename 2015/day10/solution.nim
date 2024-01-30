1fb74e480d3fb635863c3f1f44436a0a1eef7fd8
Qinsi ZHU <qszhu2006@gmail.com>
2024-01-30 15:58:18 +0800

    update

--- 2015/day10/solution.nim
+++ 2015/day10/solution.nim
@@ -9,9 +9,15 @@ import std/[
   tables,
 ]
 
-import ../utils
 
 
+iterator rle*[T](x: var openArray[T]): (T, int) =
+  var c = 0
+  for i in 0 ..< x.len:
+    c += 1
+    if i + 1 == x.len or x[i + 1] != x[i]:
+      yield (x[i], c)
+      c = 0
 
 proc next(s: string): string =
   var s = s.toSeq
--- 2015/utils.nim
+++
@@ -1,7 +0,0 @@
-iterator rle*[T](x: var openArray[T]): (T, int) =
-  var c = 0
-  for i in 0 ..< x.len:
-    c += 1
-    if i + 1 == x.len or x[i + 1] != x[i]:
-      yield (x[i], c)
-      c = 0
