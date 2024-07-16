743679f00dc52ea4ae486e74591707e1359431d4
Qinsi ZHU <qszhu2006@gmail.com>
2024-07-16 13:06:21 +0800

    2019 day23

---
+++ lib/imports.nim
@@ -0,0 +1,48 @@
+import std/[
+  algorithm,
+  bitops,
+  deques,
+  heapqueue,
+  intsets,
+  json,
+  lists,
+  math,
+  options,
+  os,
+  rdstdin,
+  re,
+  sequtils,
+  sets,
+  streams,
+  strformat,
+  strutils,
+  tables,
+  threadpool,
+  sugar,
+]
+
+import bisect, dsu, graph, grid, imos, llrb, matrix
+
+export
+  algorithm,
+  bitops,
+  deques,
+  heapqueue,
+  intsets,
+  json,
+  lists,
+  math,
+  options,
+  os,
+  rdstdin,
+  re,
+  sequtils,
+  sets,
+  streams,
+  strformat,
+  strutils,
+  tables,
+  threadpool,
+  sugar
+
+export bisect, dsu, graph, grid, imos, llrb, matrix
